3090281f2e4984042f9cfcdb7e43c882c97c2ab6
jeiiy.wang <jeiiy.wang@wingconn.com>
2019-08-06 12:35:38 +0800

    image插件 es6插件 清空dist文件夹插件

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,70 @@
 let gulp = require('gulp')
-// let concat = require('gulp-concat')
+// let concat = require('gulp-concat')     //合并文件
 // let uglify = require('gulp-uglify')
 // let rename = require('gulp-rename')
 // let less = require('gulp-less')
-// let cleanCss = require('gulp-clean-css')
-// let htmlmin = require('gulp-htmlmin')
+// let cleanCss = require('gulp-clean-css') //压缩css
+// let htmlmin = require('gulp-htmlmin')    //压缩html
 // let livereload = require('gulp-livereload') //半自动项目工具 解决手动构建 
 // let connect = require('gulp-connect') 
+let babel = require('gulp-babel') 
+let pump = require('pump') 
+let imagemin = require('gulp-imagemin'); //压缩图片
+let cache = require('gulp-cache'); //缓存图片
+let clean = require('gulp-clean'); 
 let open = require('open') 
 
 var $ = require('gulp-load-plugins')()//插件集合
 
-gulp.task('js',function(){
+gulp.task('js',['uglify_check'],function(){
     return gulp.src('src/js/*.js') //找到目标文件
     .pipe($.concat('build.js'))//合并文件
     .pipe(gulp.dest('dist/js'))//临时输出到dist/js下面
+    /** 
+     * 也可以在项目根目录中创建.babelrc 内容为：
+     * {
+     *   "presets": ["es2015"]  
+     * }
+     * 这样下面babel函数中里面就不需要传入参数
+     * 
+     * 还有安装gulp-babel一直报错提示babel-core not found 因为默认安装的是8.0.0版本 
+     * 重新安装低版本即可这里安装的是 npm i gulp-babel@7 --save-dev
+     **/
+    .pipe(babel({
+        "presets": ["es2015"]  
+      }))
     .pipe($.uglify()) //压缩
     .pipe($.rename({suffix:'.min'})) //重命名
     .pipe(gulp.dest('dist/js/'))
     .pipe($.livereload())
     .pipe($.connect.reload())
+})
+
+/**
+ * @description 检查压缩JS时的错误，作为'js'的依赖执行。
+ * 
+ * 1、解决js压缩出错的问题
+ * 2、解决修改的代码有语法错误时，服务会终止的问题
+ */
+gulp.task('uglify_check', function (cb) {
+    pump([
+        gulp.src('src/js/*.js'),
+        babel({
+            "presets": ["es2015"]
+          }),
+        $.uglify(),
+    ], cb);
+});
+
+gulp.task('image',function(){
+    gulp.src('src/img/**/*.{png,jpg,gif,ico}')
+    .pipe(cache(imagemin({
+        optimizationLevel: 5, //类型：Number 默认：5 取值范围：0-7（优化等级）
+        progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+        interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+        multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+    })))
+    .pipe(gulp.dest('dist/img'))
 })
 
 gulp.task('less',function(){
@@ -53,6 +98,13 @@ gulp.task('watch',['default'],function(){
     gulp.watch(['src/less/*.less','src/css/*.css'],['css'])
 
 })
+
+// 清空dist文件夹
+gulp.task('clean', function(){
+    return gulp.src(['/dist/*'])
+        .pipe(clean());
+});
+
 //注册全自动监视任务
 gulp.task('server',['default'],function(){
     //配置服务器选项
@@ -67,4 +119,6 @@ gulp.task('server',['default'],function(){
     open('http://localhost:5000/')
 })
 
-gulp.task('default',['js','css','html'])
+gulp.task('default',['clean'],function(){
+    gulp.start(['js','css','html','image'])
+})
